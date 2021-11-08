05e322a3bfe36b36f2644834879f658688029a26
S4KITA <lhh1120@gmail.com>
2021-11-08 22:16:52 +0900

    211108 Commit 입문 013 - 026

---
+++ NODEJS015.js
@@ -0,0 +1,9 @@
+const string = 'hello';
+const string2 = 'helelelelelelello';
+const string3 = 'The method then select elements from the start argument.';
+
+console.log('hel:', string.indexOf('hel'));
+console.log('el:', string.indexOf('el'));
+console.log('elelelel:', string2.indexOf('el'));
+console.log('method:', string3.indexOf('method'));
+console.log('bye:', string.indexOf('bye'));
---
+++ NODEJS019.js
@@ -0,0 +1,8 @@
+const user = {};
+user.name = 'Hyun Ho';
+user.age = 25;
+
+console.log(user);
+
+const user2 = {name: "kyeongrokx", age: 31};
+console.log(user2)
